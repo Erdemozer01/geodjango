33c4e92b0b91585621223d03d655298dcb66b176
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2023-08-24 06:30:40 +0300

    fix

--- world/admin.py
+++ world/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import WorldBorder
 from leaflet.admin import LeafletGeoAdmin
 
+
 # Register your models here.
 
 @admin.register(WorldBorder)
 class WorldBorderAdmin(LeafletGeoAdmin):
     list_display = ('name', "area", "lon", "lat")
-
